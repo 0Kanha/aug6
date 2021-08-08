97d96b813b1ce206801b2f1cb936a37ae381fd3d
Abhishek Jaiswal <64918997+Abhi-ML-AI@users.noreply.github.com>
2021-08-08 13:30:53 +0530

    Version 2

--- aug6_example/src/example/Try.java
+++ aug6_example/src/example/Try.java
@@ -4,7 +4,8 @@ public class Try {
 
 	public static void main(String[] args) {
 		System.out.println("This is karan!!");
-
+		System.out.println("Noice collaboration!!");
+		System.out.println("Hii");
 	}
 
 }
